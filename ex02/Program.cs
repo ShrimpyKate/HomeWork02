9d4bfe59cbe6228a32ceaa7b416af399f18ca34e
Kate <katekrupnik@gmail.com>
2023-01-09 13:50:40 +0300

    init commit

---
+++ ex02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+string n = Console.ReadLine();
+char[] array = n.ToCharArray();
+
+if (array.Length >= 3)
+  Console.WriteLine(array[2]);
+else
+  Console.WriteLine("Третьего числа нет");
